79d12c63c5ef30e882c2b1db384bc4356f8256c2
preservedfish <71352883+preservedfish@users.noreply.github.com>
2020-10-18 18:00:54 -0400

    Adjusted formatting of docstring

--- secret.py
+++ secret.py
@@ -4,8 +4,10 @@ from decouple import config
 """Before you continue, please read README.md and make sure that you followed the steps for registering an app with Spotify and Genius.
 
 There are two ways of entering the values correctly, an easy way and a hard way:
-The hard way is to create a .env file and store the values in there. For example, for the Genius access token you'd enter GENIUS_ACCESS_TOKEN=ex@mple in the .env file.
-(RECOMMENDED) The easy way is to replace the default values below with the corresponding values. For example, for the Genius access token you'd replace ex@ample with the actual token.
+The hard way is to create a .env file and store the values in there. 
+    For example, for the Genius access token you'd enter GENIUS_ACCESS_TOKEN=ex@mple in the .env file.
+(RECOMMENDED) The easy way is to replace the default values below with the corresponding values. 
+    For example, for the Genius access token you'd replace ex@ample with the actual token.
 """
 
 """Genius Client Access Token"""
